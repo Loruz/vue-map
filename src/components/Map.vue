c8c061e5de0f82aa2ccee2b8d9879bf4bd73086e
Vytautas <vpreiks@gmail.com>
2018-04-05 00:09:51 +0300

    added google map

---
+++ src/components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="map">
+		<gmap-map
+			class="google-map"
+	    	:center="center"
+	   	 	:zoom="7"
+	   		>
+		    <gmap-marker
+		  <!--     :key="index"
+		      v-for="(m, index) in markers"
+		      :position="m.position"
+		      :clickable="true"
+		      :draggable="true"
+		      @click="center=m.position" -->
+		    ></gmap-marker>
+  		</gmap-map>
+	</div>
+</template>
+<script>
+
+	export default {
+	data () {
+	      return {
+	        center: {
+	        	lat: 10.0, 
+	        	lng: 10.0
+	        },
+	        // markers: [{
+	        //   position: {lat: 10.0, lng: 10.0}
+	        // }, {
+	        //   position: {lat: 11.0, lng: 11.0}
+	        // }]
+	      }
+	   }
+	}
+</script>
+
+<style lang="scss" scoped>
+	.google-map {
+		height: 400px;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDnLvRdsXbL87_AaDSYdGLAFdyNwQKbr3Y',
+    libraries: 'places'
+  }
+})
 
 new Vue({
   el: '#app',
